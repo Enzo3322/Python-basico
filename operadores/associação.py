412740245d49bd8bf997486ccf260e21199efd53
Enzo3322 <enzo.spag14@gmail.com>
2021-01-12 13:55:19 -0300

    initial

---
+++ operadores/associação.py
@@ -0,0 +1,10 @@
+frutas = ["banana","laranja","uva","ameixa"]
+
+fruta_1 = "ameixa"
+fruta_2 = "melancia"
+
+print(fruta_1 in frutas) # True
+print(fruta_2 in frutas) # False
+
+# in 	Retorna True caso o valor seja encontrado na sequência 	2 in x
+# not in 	Retorna True caso o valor não seja encontrado na sequência 	2 not in x
---
+++ operadores/atribuição.py
@@ -0,0 +1,14 @@
+numero = 5
+
+numero += 5
+
+print(numero) # O resultado será 10
+
+    # Operador 	Exemplo 	Equivalente a
+    #   =   	x = 1   	x = 1
+    #   +=  	x += 1 	    x = x + 1
+    #   -=  	x -= 1 	    x = x - 1
+    #   *= 	    x *= 1  	x = x * 1
+    #   /=	    x /= 1  	x = x / 1
+    #   %= 	    x %= 1  	x = x % 1
+    #   %=  	x %= 1  	x = x % 1
